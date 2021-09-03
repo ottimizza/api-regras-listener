96735291d9e2f26fa52643d234a2932fa7007945
Daniel Steil <dani.steil@hotmail.com>
2021-09-03 12:17:42 -0300

    alterando topic

--- src/main/java/br/com/ottimizza/regraslistener/kafka/KafkaConsumers.java
+++ src/main/java/br/com/ottimizza/regraslistener/kafka/KafkaConsumers.java
@@ -20,7 +20,7 @@ public class KafkaConsumers {
 	@Autowired
 	RegraService service;
     
-    @KafkaListener(topics = "#{kafkaProperties.getPrefix()}ottimizza.exportar_regrasoud", 
+    @KafkaListener(topics = "#{kafkaProperties.getPrefix()}ottimizza.exportar.regras.oud", 
             	   groupId = "#{kafkaProperties.getPrefix()}tareffa-queue-events-group")
     public void integraArquivo(@Payload String message) throws JsonProcessingException, JsonMappingException {
     	
