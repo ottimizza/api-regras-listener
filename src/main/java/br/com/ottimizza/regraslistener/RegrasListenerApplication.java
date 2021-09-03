600f7654b5876b60c671a18b9e464dd68f392b5c
Daniel Steil <dani.steil@hotmail.com>
2021-09-03 11:25:59 -0300

    add anotacao eneableFeignClient

--- src/main/java/br/com/ottimizza/regraslistener/RegrasListenerApplication.java
+++ src/main/java/br/com/ottimizza/regraslistener/RegrasListenerApplication.java
@@ -2,8 +2,11 @@ package br.com.ottimizza.regraslistener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
+@EnableFeignClients
 public class RegrasListenerApplication {
 
 	public static void main(String[] args) {
