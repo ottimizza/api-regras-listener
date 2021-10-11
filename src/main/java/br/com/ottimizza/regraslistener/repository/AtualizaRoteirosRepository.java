425e3f287660f63a10106c0c7c43dfc2a486339d
Daniel Steil <dani.steil@hotmail.com>
2021-10-11 11:51:30 -0300

    ajustando anotacao

--- src/main/java/br/com/ottimizza/regraslistener/repository/AtualizaRoteirosRepository.java
+++ src/main/java/br/com/ottimizza/regraslistener/repository/AtualizaRoteirosRepository.java
@@ -3,12 +3,12 @@ package br.com.ottimizza.regraslistener.repository;
 import java.math.BigInteger;
 import java.util.List;
 
-import javax.jdo.annotations.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.ottimizza.regraslistener.domain.models.AtualizaRoteiros;
 import feign.Param;
