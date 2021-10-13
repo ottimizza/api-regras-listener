daea2c4af2dd16ef5778331b0519202821897e74
Daniel Steil <dani.steil@hotmail.com>
2021-10-13 11:27:42 -0300

    removendo logs

--- src/main/java/br/com/ottimizza/regraslistener/service/RegraService.java
+++ src/main/java/br/com/ottimizza/regraslistener/service/RegraService.java
@@ -1,7 +1,6 @@
 package br.com.ottimizza.regraslistener.service;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,13 +13,10 @@ import br.com.ottimizza.regraslistener.domain.dto.GrupoRegraDTO;
 import br.com.ottimizza.regraslistener.domain.dto.HistoricoDTO;
 import br.com.ottimizza.regraslistener.domain.dto.ObjetoInfoRoteiro;
 import br.com.ottimizza.regraslistener.domain.dto.Regra;
-import br.com.ottimizza.regraslistener.domain.dto.salesforce.SFHistorico;
-import br.com.ottimizza.regraslistener.domain.dto.salesforce.SFParticularidade;
 import br.com.ottimizza.regraslistener.domain.mapper.HistoricoMapper;
 import br.com.ottimizza.regraslistener.domain.mapper.RegraMapper;
 import br.com.ottimizza.regraslistener.domain.models.AtualizaRoteiros;
 import br.com.ottimizza.regraslistener.repository.AtualizaRoteirosRepository;
-import br.com.ottimizza.regraslistener.utils.MessageUtils;
 import br.com.ottimizza.regraslistener.utils.StringUtils;
 
 @Service
@@ -43,15 +39,10 @@ public class RegraService {
         messageString = messageString.substring(messageString.indexOf("{"), messageString.lastIndexOf("}")+ 1);
         String gpString = messageString.substring(messageString.indexOf("{") + 1, messageString.indexOf("@")-1);
         String hsString = messageString.substring(messageString.indexOf("@") + 2, messageString.lastIndexOf("}"));
-        System.out.println(gpString);
-        System.out.println(hsString);
         String[] grupoRegrasIds = gpString.split(",");
         String[] historicosIds = hsString.split(",");
-        System.out.println("gr "+grupoRegrasIds);
-        System.out.println("h "+historicosIds);
         StringBuilder regrasCRM = new StringBuilder();
         StringBuilder historicosCRM = new StringBuilder();
-        List<SFParticularidade> particularidades = new ArrayList<>();
         String cnpjContabilidade = "";
         String cnpjEmpresa = "";
         String idRoteiro = "";
@@ -82,7 +73,6 @@ public class RegraService {
             regrasCRM.append(RegraMapper.toSalesForce(grupoRegra, true).toString()+"#");
         }
         String objetoRegras = regrasCRM.toString().substring(0, regrasCRM.toString().lastIndexOf("#"));
-        System.out.println(objetoRegras);
         salesForceClient.upsertRegrasLote(CHAVE_FEIGN_CLIENT, objetoRegras);
         
         for(String idH : historicosIds) {
@@ -92,7 +82,6 @@ public class RegraService {
         }
         
         String objetoHistoricos = historicosCRM.toString().substring(0, historicosCRM.toString().lastIndexOf("#"));
-        System.out.println(objetoHistoricos);
         salesForceClient.upsertHistoricosLote(CHAVE_FEIGN_CLIENT, objetoHistoricos);
 
         ObjetoInfoRoteiro infoRoteiro = ObjetoInfoRoteiro.builder()
